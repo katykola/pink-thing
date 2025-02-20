c85ebb8276c32073b588cb558157285bc2e6019c
Katerina Kolarova <kolarkaty@gmail.com>
2025-02-20 16:46:01 +0100

    bug review text fixed

--- src/components/review.tsx
+++ src/components/review.tsx
@@ -1,7 +1,7 @@
 export default function Review() {
     return (
         <div className='flex flex-col gap-8'>
-        <p className='text-center opacity-100 leading-7'>"A visual guide could be a wireframe, creative composition, or information architecture. A device that enables collaboration will lessen the chance of work having to be completely redone. A manager could create a visual guide for the team to complete the build-out."</p>
+        <p className='text-center opacity-100 leading-7'>A visual guide could be a wireframe, creative composition, or information architecture. A device that enables collaboration will lessen the chance of work having to be completely redone. A manager could create a visual guide for the team to complete the build-out.</p>
         <div>
           <p className='text-center text-lg text-dark font-bold'>Matt Cannon</p>
           <p className='text-center text-sm'>Head of Marketing</p>
