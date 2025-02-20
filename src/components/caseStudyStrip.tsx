f8d67d20e95553cfaa41c317a5ab15adaf74147d
Katerina Kolarova <kolarkaty@gmail.com>
2025-02-20 15:21:44 +0100

    props image case study strip fixed

--- src/components/caseStudyStrip.tsx
+++ src/components/caseStudyStrip.tsx
@@ -1,11 +1,12 @@
 import CaseStudy from '@/components/caseStudy';
+import LaptopMockup from '/public/images/laptop-mockup.webp';
 
 export default function CaseStudyStrip() {
     return(
         <div className='grid grid-cols-3 gap-8'>
-            <CaseStudy />
-            <CaseStudy />
-            <CaseStudy />
+            <CaseStudy image={LaptopMockup}/>
+            <CaseStudy image={LaptopMockup}/>
+            <CaseStudy image={LaptopMockup}/>
         </div>
     )
 }
